80cd6f4e52614eadab82b78864e051284793a624
roboaiapaths99 <roboaiapaths99@gmail.com>
2026-06-08 14:31:41 +0530

    fix: resolve relative logger path in cleanup.service.js

--- src/modules/finance/shared/services/cleanup.service.js
+++ src/modules/finance/shared/services/cleanup.service.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const logger = require('../../../core/logger');
+const logger = require('../../../../core/logger');
 
 class CleanupService {
     /**
